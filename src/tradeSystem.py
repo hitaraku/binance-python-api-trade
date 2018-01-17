2dd7a2479f9db4daa639d01b11ff139e1ac819ef
tNakajima <airu85@live.jp>
2018-01-17 19:23:36 +0900

    First Commit

---
+++ src/tradeSystem.py
@@ -0,0 +1,186 @@
+import configparser
+import json
+from binance.client import Client 
+import time
+import re
+
+from datetime import datetime
+
+from binance.enums import *
+from binance.exceptions import *
+
+def buyCoin( symbol, afterPrice, tickerCount ):
+    try:
+        print("called buy")
+        balance = client.get_asset_balance(asset='BTC')
+        orderQuantity = round(float(balance['free']) / (float(afterPrice) * 1.01))
+        print(orderQuantity)
+        print(afterPrice)
+
+        order = client.order_limit_buy(
+                symbol=symbol,
+                price=afterPrice,
+                quantity=orderQuantity)
+
+        """ [TEST]
+        order = client.order_limit_buy(
+            symbol="TRXBTC",
+            price='0.00000995',
+            quantity='2000')
+
+
+
+            order = client.create_test_order(
+            symbol=symbol,
+            side=SIDE_BUY,
+            type=ORDER_TYPE_LIMIT,
+            timeInForce=TIME_IN_FORCE_GTC,
+            price=afterPrice,
+            quantity=orderQuantity)
+        """ 
+
+        count = 0
+        while 1:
+            time.sleep(1)
+            checkOrder = client.get_order(
+                symbol=symbol,
+                orderId=order['orderId'])
+            print(checkOrder['status'])
+            if str(checkOrder['status']) == "FILLED":
+                break
+            if str(checkOrder['status']) == "NEW":
+                count = 1 + count
+            if count > 10:
+                # cancel order
+                result = client.cancel_order(
+                    symbol=symbol,
+                    orderId=checkOrder['orderId'])
+                print(result)
+                return
+
+        # bougut complete
+        print("bought")
+        time.sleep(10)
+        print("bought end")
+        checkOrder = client.get_order(
+            symbol=symbol,
+            orderId=order['orderId'])
+        sellCoin( symbol, tickerCount, checkOrder['origQty'] )
+        print("sell end")
+        return
+
+    except BinanceAPIException as e:
+        print(e.status_code)
+        print(e.message)
+        return
+
+
+
+def sellCoin( symbol, tickerCount, orderQuantity ): 
+    try:
+        print("called sell")
+        prices = client.get_all_tickers()
+        print(prices[tickerCount]['price'])
+        print("orderQuantity: " + orderQuantity)
+
+        floatSymbolValue = float(client.get_symbol_info(symbol=symbol)['filters'][1]['minQty'])
+        print(floatSymbolValue)
+
+        count = 0
+        while 1:
+            qtyCount = floatSymbolValue * math.pow(10, count)
+            if qtyCount >= 1.0:
+                break
+            count += 1
+        
+        # This code trim "BTC" string from symbol
+        balance = client.get_asset_balance(asset=symbol.replace("BTC", ""))
+
+        # calucrate quantity from ticker quantity
+        amount = float(balance['free'])
+        precision = count
+        amt_str = "{:0.0{}f}".format(amount, precision)
+        print(float(amt_str))
+
+        # client.get_symbol_info(symbol=symbol)['filters'][1]['minQty']
+
+        sellOrder = client.order_limit_sell(
+            symbol=symbol,
+            price=prices[tickerCount]['price'],
+            quantity=float(amt_str))
+
+        """ [TEST]
+        order = client.create_test_order(
+            symbol=symbol,
+            side=SIDE_SELL,
+            type=ORDER_TYPE_LIMIT,
+            timeInForce=TIME_IN_FORCE_GTC,
+            price=prices[tickerCount]['price'],
+            quantity=orderQuantity)
+
+        """
+
+        while 1:
+            time.sleep(1)
+            checkOrder = client.get_order(
+                symbol=symbol,
+                orderId=sellOrder['orderId'])
+            print(checkOrder['status'])
+            if str(checkOrder['status']) == "FILLED":
+                break
+        print("sold")
+        return
+
+    except BinanceAPIException as e:
+        print(e.status_code)
+        print(e.message)
+        return
+
+# load config
+inifile = configparser.ConfigParser()
+inifile.read('./config/config.ini', 'UTF-8')
+ 
+# get APIKEY
+api_key = inifile.get('settings', 'API_KEY')
+api_secret = inifile.get('settings', 'API_SECRET')
+
+# set binance client APIKEY
+client = Client(api_key, api_secret)
+
+# get all symbol prices
+prices = client.get_all_tickers()
+symbolLen = len(prices)
+
+print(prices[0]['symbol'])
+
+# before, after prices
+beforePrices = [prices[i]['price'] for i in range(symbolLen)]
+afterPrices = [0 for i in range(symbolLen)]
+
+# log to file
+file_object  = open("./log/" + datetime.now().strftime("%Y%m%d_%H%M%S")+"_log.txt", "w") 
+
+while 1:
+    # print("start func")
+    balance = client.get_asset_balance(asset='BTC')
+    print("free myBTC : " + str(balance['free']))
+    time.sleep(7)
+    prices = client.get_all_tickers()
+    for i in range(symbolLen):
+        afterPrices[i] = prices[i]['price']
+        # check 5% down 
+        pricePropotion = 100 - (float(afterPrices[i]) / float(beforePrices[i])) * 100
+        # buy 5% down ticker 
+        if ( pricePropotion ) > 5.0: 
+            # Ticker BTC
+            if re.search(r'BTC', prices[i]['symbol']) and (round (float(balance['free']) / (float(afterPrices[i]) * 1.03)) ) > 0.0:
+                # write file
+                file_object.write('time: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ' , ticker: ' + prices[i]['symbol'] + ' , PricePropotion: ' + str(pricePropotion) + ' , afterprice: ' + afterPrices[i] + ' , beforePrice: ' + beforePrices[i] + "\n")
+                print('time: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ' , ticker: ' + prices[i]['symbol'] + ' , PricePropotion: ' + str(pricePropotion) + ' , afterprice: ' + afterPrices[i] + ' , beforePrice: ' + beforePrices[i])
+                buyCoin(prices[i]['symbol'], afterPrices[i], i)
+                break
+
+    for i in range(symbolLen):
+        beforePrices[i] = prices[i]['price']
+
+file_object.close()
